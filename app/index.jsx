3f9f20b1e185e29dacf9c2997e1e2a784b6e009d
Masakazu Yasumoto <yasumotom@wit.edu>
2024-09-22 16:32:50 -0400

    Initial Changes for Bottom Tabs

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import { Stack } from 'expo-router/stack';
+import {SafeAreaView} from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Moosa!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style = {{color: 'blue'}}>Go to Profile</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+
+    </SafeAreaView>
   );
 }
 
