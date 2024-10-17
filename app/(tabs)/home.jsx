03524a939e0e8e0f92ab542b955bfe2f2198c7f7
Masakazu Yasumoto <yasumotom@wit.edu>
2024-10-17 16:48:55 -0400

    View uploaded videos list

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,27 +1,33 @@
 import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {images} from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
+import { getAllPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
 const Home = () => {
+
+  const { data: posts, isLoading, refetch } = useAppwrite(getAllPosts);
 
   const [refreshing, setRefreshing] = useState(false)
   const onRefresh = async () => {
     setRefreshing(true);
     // recall videos -> if any new videos appear
+    await refetch();
     setRefreshing(false);
   }
   return (
     <SafeAreaView className="bg-primary h-full">
       {/* list of elements */}
       <FlatList 
-        data={[{id: 1 }, {id: 2}, {id: 3},]}
+        data={posts}
         // data = {[]}
         keyExtractor={(item) => item.id}
         renderItem={({item}) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <VideoCard video={item}/>
         )}
 
         ListHeaderComponent={() => (
