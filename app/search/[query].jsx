ecc0c84dd63fcfa6a51a17e3f560dbed4f7a6233
Masakazu Yasumoto <yasumotom@wit.edu>
2024-10-21 16:28:50 -0400

    Show search results

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+import {  searchPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
+import { useLocalSearchParams } from 'expo-router'
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query])
+  // console.log(posts)
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      {/* list of elements */}
+      <FlatList 
+        data={posts}
+        // data = {[]}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => (
+          <VideoCard video={item}/>
+        )}
+
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Results
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">
+              {query}
+            </Text> 
+
+          {/* <View className="mt-1.5">
+            <Image 
+              source={images.logoSmall}
+              className="w-9 h-10"
+              resizeMode='contain'/>
+          </View> */}
+
+            <View className="mt-6 mb-8">
+              <SearchInput initialQuery={query}/>
+            </View>
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos 
+              </Text>
+
+              <Trending posts={ posts ?? []}/>
+            </View>
+          </View>
+
+        )}
+        
+        // What happens when the list is empty
+        ListEmptyComponent={() => (
+          <EmptyState 
+            title="No Videos Found!"
+            subtitle="No videos found for this search query"
+          />
+        )}
+
+      /> 
+    </SafeAreaView>
   )
 }
 
